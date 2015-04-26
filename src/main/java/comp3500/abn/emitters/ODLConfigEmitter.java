a95136bc67e3d5ece01524b8c86c519f6ad276fa
Benjamin Roberts <benjamin.roberts@anu.edu.au>
2015-04-26 14:50:35 +1000

    BGPPeers are now parsed, created and emitted.
    BGPSpeaker objects can now parse their RpslObject for import, export and default attributes.
    These are used to determine which BGPPeers need to be created (currently 1 per ASN).
    The template and emitter has been updated to use this new object.
    Routes and multiple IP address exports are not currently implemented.

--- src/main/java/comp3500/abn/emitters/ODLConfigEmitter.java
+++ src/main/java/comp3500/abn/emitters/ODLConfigEmitter.java
@@ -1,33 +1,53 @@
+/*
+ * Copyright (c) 2015 Benjamin Roberts, Nathan Kelly, Andrew Maxwell
+ * All rights reserved.
+ */
+
 package comp3500.abn.emitters;
 
 import java.io.StringWriter;
+import java.util.HashSet;
 import java.util.Set;
 
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
+
+import comp3500.abn.emitters.odlconfig.BGPPeer;
+import comp3500.abn.emitters.odlconfig.BGPSpeaker;
 import net.ripe.db.whois.common.rpsl.RpslObject;
 
+/**
+ * Creates and emits an OpenDaylight BGP configuration file.
+ * @author Benjamin George Roberts
+ */
 public class ODLConfigEmitter implements OutputEmitter {
 	private static final String TEMPLATE_RESOURCE = "mustache/ODLConfigEmitter.mustache"; 
 	
+	
+	Set<BGPSpeaker> bgpServers = new HashSet<BGPSpeaker>();
+	Set<BGPPeer> 	 bgpPeers 	= new HashSet<BGPPeer>();
+	
 	/*
-	 * BGP Config file constants. These are configured by passing emitter arguments.
+	 * BGP config file constants. 
+	 * These are configured by passing emitter arguments and are included in the template.
 	 */
-	//speaker/peer configuration
-	public String  BGP_PEER_REGISTRY = "peer-registry",
-					BGP_DISPATCHER = "global-bgp-dispatcher"; 
-	// BGP connection attributes 
-	public int  BGP_RECONNECT_SLEEP_MIN = 1000,
-				 BGP_RECONNECT_SLEEP_MAX = 180000,
-				 BGP_RECONNCET_CONNECT_TIME = 5000;
-	public double BGP_RECONNCET_SLEEP_FACTOR = 2.0;
-	public String BGP_RECONNECT_STRATEGY_NAME = "reconnect-strategy-factory",
-			       BGP_EXECUTOR_NAME = "global-event-executor";
+	String 	BGP_PEER_REGISTRY 			= "peer-registry",
+	      	BGP_DISPATCHER 				= "global-bgp-dispatcher",
+  			BGP_RECONNECT_STRATEGY_NAME = "reconnect-strategy-factory",
+	      	BGP_EXECUTOR_NAME 			= "global-event-executor";
+	int		BGP_RECONNECT_SLEEP_MIN 	= 1000,
+			BGP_RECONNECT_SLEEP_MAX 	= 180000,
+			BGP_RECONNCET_CONNECT_TIME 	= 5000,
+			BGP_HOLD_TIME				= 180;
+	double 	BGP_RECONNCET_SLEEP_FACTOR 	= 2.0;
 	
 	@Override
 	public String emit(Set<RpslObject> rpslObjects) {
 		Mustache templateRenderer = (new DefaultMustacheFactory()).compile(TEMPLATE_RESOURCE);
 		StringWriter templateWriter = new StringWriter();
+		
+		//Parse the objects and build the object lists
+		parseObjects(rpslObjects);
 		
 		//Render template and flush stream
 		templateRenderer.execute(templateWriter, this);
@@ -58,6 +78,7 @@ public class ODLConfigEmitter implements OutputEmitter {
 						case "BGP_RECONNCET_SLEEP_FACTOR": BGP_RECONNCET_SLEEP_FACTOR = Double.parseDouble(splitArguments[1]); break;
 						case "BGP_EXECUTOR_NAME": BGP_EXECUTOR_NAME = splitArguments[1]; break;
 						case "BGP_RECONNECT_STRATEGY_NAME": BGP_RECONNECT_STRATEGY_NAME = splitArguments[1]; break;
+						case "BGP_HOLD_TIME": BGP_HOLD_TIME = Integer.parseInt(splitArguments[1]); break;
 						default:
 							System.err.println("Unknown argument: " + argument);
 							break;
@@ -66,6 +87,24 @@ public class ODLConfigEmitter implements OutputEmitter {
 					//We failed to parse an integer or double
 					System.err.println("Failed to parse value: " + argument);
 				}
+			}
+		}
+	}
+	
+	/**
+	 * Parse the RPSLObject stream by creating {@link BGPSpeaker}s from AUT_NUM {@link RpslObject}s,
+	 * and getting the set of their peers.
+	 * @param objects {@link RpslObject}s to parse
+	 */
+	private void parseObjects(Set<RpslObject> objects) {
+		for(RpslObject object: objects) {
+			switch(object.getType()) {
+				case AUT_NUM:
+					BGPSpeaker speaker = new BGPSpeaker(object);
+					bgpServers.add(speaker);
+					bgpPeers.addAll(speaker.getPeers());
+					break;
+				default: break; //Ignore non handled attributes	
 			}
 		}
 	}
