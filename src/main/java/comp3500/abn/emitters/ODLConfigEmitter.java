abn3500/rpsl-parser

Added skeleton implementation of new ODLConfigEmitter
The new ODLConfigEmitter generates the sets of objects that will be
written in the template, has an "empty" template and invokes the
template engine.

BGPInetRtr was also modified to provide public access to a copy of it's
peers

src/main/java/comp3500/abn/emitters/ODLConfigEmitter.java
@@ -1,0 +1,117 @@
+/*
+ * Copyright (c) 2015 Benjamin Roberts, Nathan Kelly, Andrew Maxwell
+ * All rights reserved.
+ */
+
+package comp3500.abn.emitters;
+
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+
+import comp3500.abn.emitters.odlconfig.BGPAutNum;
+import comp3500.abn.emitters.odlconfig.BGPInetRtr;
+import comp3500.abn.emitters.odlconfig.BGPPeer;
+import net.ripe.db.whois.common.rpsl.AttributeType;
+import net.ripe.db.whois.common.rpsl.ObjectType;
+import net.ripe.db.whois.common.rpsl.RpslObject;
+
+public class ODLConfigEmitter implements OutputEmitter {
+	private static final String TEMPLATE_RESOURCE = "mustache/ODLConfigEmitter.mustache"; 
+	private Set<BGPInetRtr> speakerSet;
+	private Set<BGPPeer> peerSet;
+
+	@Override
+	public String emit(Set<RpslObject> objects) {	
+		//Generate the speaker and autnum sets
+		speakerSet = generateSpeakers(objects, generateAutNums(objects));
+		
+		//Generate the peer set
+		peerSet = generatePeers(speakerSet);
+		
+		//Instantiate the template engine and write the config
+		Mustache templateRenderer = (new DefaultMustacheFactory()).compile(TEMPLATE_RESOURCE);
+		StringWriter templateWriter = new StringWriter();
+		
+		templateRenderer.execute(templateWriter, this);
+		templateWriter.flush();
+		
+		return templateWriter.toString();
+	}
+
+	@Override
+	public void setArguments(Map<String, String> arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * Generate the set of peers declared by {@link BGPInetRtr}s.
+	 * @param speakers Set of {@link BGPInetRtr}s
+	 * @return Set of declared {@link BGPPeer}s
+	 */
+	static Set<BGPPeer> generatePeers(Set<BGPInetRtr> speakers) {
+		HashSet<BGPPeer> peerSet = new HashSet<BGPPeer>();
+
+		for(BGPInetRtr speaker : speakers) {
+			peerSet.addAll(speaker.getPeers());
+		}
+		
+		return peerSet;
+	}
+	
+	/**
+	 * Generate the set of {@link BGPInetRtr}s declared in the RPSL document.
+	 * Peers must be members of declared {@link BGPAutNum} objects.
+	 * @param objects Set of {@link RpslObject}s
+	 * @param autNumMap Map of AS Numbers to {@link BGPAutNum}s to instantiate {@link BGPInetRtr} from
+	 * @return Set of declared {@link BGPInetRtr}s
+	 */
+	static Set<BGPInetRtr> generateSpeakers(Set<RpslObject> objects, Map<String, BGPAutNum> autNumMap) {		
+		HashSet<BGPInetRtr> speakerSet = new HashSet<BGPInetRtr>();
+		
+		//Iterate through object set to find inet-rtr objects
+		for(RpslObject o : objects) {
+			if(o.getType() != ObjectType.INET_RTR)
+				continue;
+			
+			//get AS of inet-rtr
+			String localAS = o.getValueForAttribute(AttributeType.LOCAL_AS).toString();
+			
+			if(!autNumMap.containsKey(localAS))
+				continue; //TODO handle this case better
+			
+			speakerSet.addAll(BGPInetRtr.getSpeakerInstances(o, autNumMap.get(localAS)));
+		}
+		
+		return speakerSet;
+
+	}
+	
+	/**
+	 * Instantiates {@link BGPAutNum} objects for each "aut-num" RPSL object
+	 * in the provided set.
+	 * @param objects Set of {@link RpslObject}s
+	 * @return Map of AS Numbers to {@link BGPAutNum}s as declared in objects
+	 */
+	static Map<String, BGPAutNum> generateAutNums(Set<RpslObject> objects) {
+		HashMap<String, BGPAutNum> autNumMap = new HashMap<String, BGPAutNum>();
+		
+		//Iterate through object set to find aut-num objects
+		for(RpslObject o: objects) {
+			if(o.getType() != ObjectType.AUT_NUM)
+				continue;
+			String asNumber = o.getTypeAttribute().getCleanValue().toString();
+			
+			//TODO check and handle case where asNumber is already inserted
+			autNumMap.put(asNumber, new BGPAutNum(o));
+		}
+		
+		return autNumMap;
+	}
+}

src/test/java/comp3500/abn/emitters/ODLConfigEmitterTest.java
@@ -1,0 +1,102 @@
+/*
+ * Copyright (c) 2015 Benjamin Roberts, Nathan Kelly, Andrew Maxwell
+ * All rights reserved.
+ */
+
+package comp3500.abn.emitters;
+
+import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import net.ripe.db.whois.common.io.RpslObjectStringReader;
+import net.ripe.db.whois.common.rpsl.RpslObject;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import comp3500.abn.emitters.odlconfig.BGPAutNum;
+import comp3500.abn.emitters.odlconfig.BGPInetRtr;
+import comp3500.abn.emitters.odlconfig.BGPPeer;
+
+public class ODLConfigEmitterTest {
+	private final String AUTNUM_EXAMPLE = "aut-num: AS1\n"
+			+ "as-name: First AS\n"
+			+ "export: to AS3 2.2.2.1 at 1.1.1.1 announce 3.3.3.0/24\n\n"
+			+ "aut-num: AS2\n"
+			+ "as-name: Second AS\n"
+			+ "export: to AS3 2.2.2.2 2.2.2.3 at 1.1.1.1 announce 3.3.3.0/24\n\n";
+	private final String SPEAKER_EXAMPLE = "inet-rtr: router1\n"
+			+ "local-as: AS1\n"
+			+ "ifaddr: 1.1.1.1 masklen 24\n"
+			+ "peer: BGP4 2.2.2.1\n\n"
+			+ "inet-rtr: router2\n"
+			+ "local-as: AS2\n"
+			+ "ifaddr: 1.1.1.2 masklen 24\n"
+			+ "ifaddr: 1.1.1.3 masklen 24\n"
+			+ "peer: BGP4 2.2.2.2\n"
+			+ "peer: BGP4 2.2.2.3\n\n";
+	
+	private ODLConfigEmitter e;
+
+	@Before
+	public void newEmitter() {
+		e = new ODLConfigEmitter();
+	}
+	
+	@Test
+	public void outputSanityTest() {
+		assertTrue("Emitter outputs some sort of string",
+				e.emit(getObjects(AUTNUM_EXAMPLE + SPEAKER_EXAMPLE)).length() > 0);
+	}
+	
+	@Test
+	public void generatePeers() {
+		Set<RpslObject> objects = getObjects(AUTNUM_EXAMPLE + SPEAKER_EXAMPLE);
+		Map<String, BGPAutNum> autNums = ODLConfigEmitter.generateAutNums(objects);
+		Set<BGPPeer> peerSet = ODLConfigEmitter.generatePeers(
+				ODLConfigEmitter.generateSpeakers(objects, autNums));
+		
+		assertEquals("Should generate a peer object for peer of each speaker declared in the RPSL document", 5, peerSet.size());
+		
+	}
+	
+	@Test
+	public void generatesAutNums() {
+		Set<RpslObject> objects = getObjects(AUTNUM_EXAMPLE);
+		
+		Map<String, BGPAutNum> autNums = ODLConfigEmitter.generateAutNums(objects);
+		
+		assertEquals("Should generate autnum object for each in rpsl document", 2, autNums.size());
+		assertTrue("AutNum Map should contain AS from RPSL document", autNums.containsKey("AS1"));
+		assertTrue("AutNum Map should contain AS from RPSL document", autNums.containsKey("AS2"));
+	}
+	
+	@Test 
+	public void generatesSpeakers() {
+		Set<RpslObject> objects = getObjects(AUTNUM_EXAMPLE + SPEAKER_EXAMPLE);
+		
+		Map<String, BGPAutNum> autNums = ODLConfigEmitter.generateAutNums(objects);
+		Set<BGPInetRtr> speakerSet = ODLConfigEmitter.generateSpeakers(objects, autNums);
+		
+		assertEquals("Should generate speaker for each interface of RPSL inet-rtrs", 3, speakerSet.size());
+	}
+	
+	/**
+	 * Parse an RPSL document (multiple objects) and return the set
+	 * of declared objects
+	 * @param rpslString rpsl document as string
+	 * @return Set of {@link RpslObject}s
+	 */
+	private static Set<RpslObject> getObjects(String rpslString) {
+		Set<RpslObject> rpslSet = new HashSet<RpslObject>();
+
+		for(String objString : (new RpslObjectStringReader(rpslString)))
+			rpslSet.add(RpslObject.parse(objString));
+		
+		return rpslSet;
+	}
+
+}

rpsl4j/rpsl4j-opendaylight

src/main/java/comp3500/abn/emitters/ODLConfigEmitter.java
@@ -1,0 +1,118 @@
+/*
+ * Copyright (c) 2015 Benjamin Roberts, Nathan Kelly, Andrew Maxwell
+ * All rights reserved.
+ */
+
+package comp3500.abn.emitters;
+
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+
+import org.rpsl4j.emitters.OutputEmitter;
+import org.rpsl4j.emitters.rpsldocument.BGPAutNum;
+import org.rpsl4j.emitters.rpsldocument.BGPInetRtr;
+import org.rpsl4j.emitters.rpsldocument.BGPPeer;
+import net.ripe.db.whois.common.rpsl.AttributeType;
+import net.ripe.db.whois.common.rpsl.ObjectType;
+import net.ripe.db.whois.common.rpsl.RpslObject;
+
+public class ODLConfigEmitter implements OutputEmitter {
+	private static final String TEMPLATE_RESOURCE = "mustache/ODLConfigEmitter.mustache";
+	private Set<BGPInetRtr> speakerSet;
+	private Set<BGPPeer> peerSet;
+
+	@Override
+	public String emit(Set<RpslObject> objects) {
+		//Generate the speaker and autnum sets
+		speakerSet = generateSpeakers(objects, generateAutNums(objects));
+
+		//Generate the peer set
+		peerSet = generatePeers(speakerSet);
+
+		//Instantiate the template engine and write the config
+		Mustache templateRenderer = (new DefaultMustacheFactory()).compile(TEMPLATE_RESOURCE);
+		StringWriter templateWriter = new StringWriter();
+
+		templateRenderer.execute(templateWriter, this);
+		templateWriter.flush();
+
+		return templateWriter.toString();
+	}
+
+	@Override
+	public void setArguments(Map<String, String> arguments) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Generate the set of peers declared by {@link BGPInetRtr}s.
+	 * @param speakers Set of {@link BGPInetRtr}s
+	 * @return Set of declared {@link BGPPeer}s
+	 */
+	static Set<BGPPeer> generatePeers(Set<BGPInetRtr> speakers) {
+		HashSet<BGPPeer> peerSet = new HashSet<BGPPeer>();
+
+		for(BGPInetRtr speaker : speakers) {
+			peerSet.addAll(speaker.getPeers());
+		}
+
+		return peerSet;
+	}
+
+	/**
+	 * Generate the set of {@link BGPInetRtr}s declared in the RPSL document.
+	 * Peers must be members of declared {@link BGPAutNum} objects.
+	 * @param objects Set of {@link RpslObject}s
+	 * @param autNumMap Map of AS Numbers to {@link BGPAutNum}s to instantiate {@link BGPInetRtr} from
+	 * @return Set of declared {@link BGPInetRtr}s
+	 */
+	static Set<BGPInetRtr> generateSpeakers(Set<RpslObject> objects, Map<String, BGPAutNum> autNumMap) {
+		HashSet<BGPInetRtr> speakerSet = new HashSet<BGPInetRtr>();
+
+		//Iterate through object set to find inet-rtr objects
+		for(RpslObject o : objects) {
+			if(o.getType() != ObjectType.INET_RTR)
+				continue;
+
+			//get AS of inet-rtr
+			String localAS = o.getValueForAttribute(AttributeType.LOCAL_AS).toString();
+
+			if(!autNumMap.containsKey(localAS))
+				continue; //TODO handle this case better
+
+			speakerSet.addAll(BGPInetRtr.getSpeakerInstances(o, autNumMap.get(localAS)));
+		}
+
+		return speakerSet;
+
+	}
+
+	/**
+	 * Instantiates {@link BGPAutNum} objects for each "aut-num" RPSL object
+	 * in the provided set.
+	 * @param objects Set of {@link RpslObject}s
+	 * @return Map of AS Numbers to {@link BGPAutNum}s as declared in objects
+	 */
+	static Map<String, BGPAutNum> generateAutNums(Set<RpslObject> objects) {
+		HashMap<String, BGPAutNum> autNumMap = new HashMap<String, BGPAutNum>();
+
+		//Iterate through object set to find aut-num objects
+		for(RpslObject o: objects) {
+			if(o.getType() != ObjectType.AUT_NUM)
+				continue;
+			String asNumber = o.getTypeAttribute().getCleanValue().toString();
+
+			//TODO check and handle case where asNumber is already inserted
+			autNumMap.put(asNumber, new BGPAutNum(o));
+		}
+
+		return autNumMap;
+	}
+}

src/test/java/comp3500/abn/emitters/ODLConfigEmitterTest.java
@@ -1,0 +1,102 @@
+/*
+ * Copyright (c) 2015 Benjamin Roberts, Nathan Kelly, Andrew Maxwell
+ * All rights reserved.
+ */
+
+package comp3500.abn.emitters;
+
+import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import net.ripe.db.whois.common.io.RpslObjectStringReader;
+import net.ripe.db.whois.common.rpsl.RpslObject;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.rpsl4j.emitters.rpsldocument.BGPAutNum;
+import org.rpsl4j.emitters.rpsldocument.BGPInetRtr;
+import org.rpsl4j.emitters.rpsldocument.BGPPeer;
+
+public class ODLConfigEmitterTest {
+	private final String AUTNUM_EXAMPLE = "aut-num: AS1\n"
+			+ "as-name: First AS\n"
+			+ "export: to AS3 2.2.2.1 at 1.1.1.1 announce 3.3.3.0/24\n\n"
+			+ "aut-num: AS2\n"
+			+ "as-name: Second AS\n"
+			+ "export: to AS3 2.2.2.2 2.2.2.3 at 1.1.1.1 announce 3.3.3.0/24\n\n";
+	private final String SPEAKER_EXAMPLE = "inet-rtr: router1\n"
+			+ "local-as: AS1\n"
+			+ "ifaddr: 1.1.1.1 masklen 24\n"
+			+ "peer: BGP4 2.2.2.1\n\n"
+			+ "inet-rtr: router2\n"
+			+ "local-as: AS2\n"
+			+ "ifaddr: 1.1.1.2 masklen 24\n"
+			+ "ifaddr: 1.1.1.3 masklen 24\n"
+			+ "peer: BGP4 2.2.2.2\n"
+			+ "peer: BGP4 2.2.2.3\n\n";
+
+	private ODLConfigEmitter e;
+
+	@Before
+	public void newEmitter() {
+		e = new ODLConfigEmitter();
+	}
+
+	@Test
+	public void outputSanityTest() {
+		assertTrue("Emitter outputs some sort of string",
+				e.emit(getObjects(AUTNUM_EXAMPLE + SPEAKER_EXAMPLE)).length() > 0);
+	}
+
+	@Test
+	public void generatePeers() {
+		Set<RpslObject> objects = getObjects(AUTNUM_EXAMPLE + SPEAKER_EXAMPLE);
+		Map<String, BGPAutNum> autNums = ODLConfigEmitter.generateAutNums(objects);
+		Set<BGPPeer> peerSet = ODLConfigEmitter.generatePeers(
+				ODLConfigEmitter.generateSpeakers(objects, autNums));
+
+		assertEquals("Should generate a peer object for peer of each speaker declared in the RPSL document", 5, peerSet.size());
+
+	}
+
+	@Test
+	public void generatesAutNums() {
+		Set<RpslObject> objects = getObjects(AUTNUM_EXAMPLE);
+
+		Map<String, BGPAutNum> autNums = ODLConfigEmitter.generateAutNums(objects);
+
+		assertEquals("Should generate autnum object for each in rpsl document", 2, autNums.size());
+		assertTrue("AutNum Map should contain AS from RPSL document", autNums.containsKey("AS1"));
+		assertTrue("AutNum Map should contain AS from RPSL document", autNums.containsKey("AS2"));
+	}
+
+	@Test
+	public void generatesSpeakers() {
+		Set<RpslObject> objects = getObjects(AUTNUM_EXAMPLE + SPEAKER_EXAMPLE);
+
+		Map<String, BGPAutNum> autNums = ODLConfigEmitter.generateAutNums(objects);
+		Set<BGPInetRtr> speakerSet = ODLConfigEmitter.generateSpeakers(objects, autNums);
+
+		assertEquals("Should generate speaker for each interface of RPSL inet-rtrs", 3, speakerSet.size());
+	}
+
+	/**
+	 * Parse an RPSL document (multiple objects) and return the set
+	 * of declared objects
+	 * @param rpslString rpsl document as string
+	 * @return Set of {@link RpslObject}s
+	 */
+	private static Set<RpslObject> getObjects(String rpslString) {
+		Set<RpslObject> rpslSet = new HashSet<RpslObject>();
+
+		for(String objString : (new RpslObjectStringReader(rpslString)))
+			rpslSet.add(RpslObject.parse(objString));
+
+		return rpslSet;
+	}
+
+}